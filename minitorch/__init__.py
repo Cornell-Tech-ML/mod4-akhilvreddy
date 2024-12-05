2bec9cb8ae0e705ed345ec4afdaf30c875fae1ef
Akhil Reddy <avr54@scarletmail.rutgers.edu>
2024-12-05 09:30:51 -0500

    made some changes

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,3 +1,8 @@
+"""The minitorch package.
+
+Provides utilities for lightweight neural network implementations.
+"""
+
 from .testing import MathTest, MathTestVariable  # type: ignore # noqa: F401,F403
 from .datasets import *  # noqa: F401,F403
 from .optim import *  # noqa: F401,F403
